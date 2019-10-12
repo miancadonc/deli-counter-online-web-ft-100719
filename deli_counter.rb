209f48609b1ecaf5a9823f1f91c7d89e415c43af
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-12 23:31:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ end
 
 def take_a_number(array, name)
   array << name
-  puts "Welcome, #{name}. You are number #{array.find}"
+  puts "Welcome, #{name}. You are number #{(array.find_index(name))+1} in line."
 end
 
 def now_serving(array)
