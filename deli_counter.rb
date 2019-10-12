419c4f7ec185fc8130d7ef77210a769ae350c127
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-12 23:25:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@ katz_deli = []
 
 def line(array)
   line = ""
-  line << array.each_with_index {|x, i| "#{i+1}. #{x}"}
+  line << array.each_with_index {|x, i| " #{i+1}. #{x}"}
+  puts ""
 end
 
 def take_a_number(array, name)
